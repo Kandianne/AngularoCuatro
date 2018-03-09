d2f974afff50ace2774b00bcbd6b3302dd99dcbd
TIMMONSAD01\kandi.pierre <kandi.pierre@timmons.com>
2018-03-09 16:40:52 -0500

    adding interfaces

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { IProduct } from "./product";
 
 @Component({
     selector: 'pm-products',
@@ -6,7 +7,11 @@ import { Component } from "@angular/core";
 })
 export class ProductListComponent {
     viewTitle: string = 'Product List';
-    products: any[] = [
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
+    listFilter: string = 'cart';
+    products: IProduct[] = [
         {
             "productId": 1,
             "productName": "Leaf Rake",
@@ -38,4 +43,8 @@ export class ProductListComponent {
             "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
         }
     ];
+
+    toggleImage(): void{
+        this.showImage = !this.showImage;
+    }
 }
